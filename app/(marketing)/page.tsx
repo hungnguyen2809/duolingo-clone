59e3d424ddace80c819410224612601880792dd4
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2024-05-06 00:30:32 +0700

    1

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+function MartketingPage() {
+  return (
+    <div className="p-4">
+      <p>Marketing Page</p>
+
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default MartketingPage;
